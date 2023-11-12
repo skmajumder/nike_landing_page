63c355915e32f341ea1fb212ac31d785b8981b0d
skmajumder <skmajumder.cse@gmail.com>
2023-11-12 21:51:22 +0600

    section define

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ const App = () => {
   return (
     <>
       <main className="relative">
-        <section className="xl:padding-l wide:padding-r padding-b"></section>
+        <section className="xl:padding-l wide:padding-r padding-b">
+          Hero
+        </section>
+        <section className="padding">PopularProducts</section>
+        <section className="padding">SuperQuality</section>
+        <section className="padding-x py-10">Services</section>
+        <section className="padding">SpecialOffer</section>
+        <section className="padding bg-pale-blue">CustomerReview</section>
+        <section className="padding-x w-full py-16 sm:py-32">Subscribe</section>
+        <footer className="padding-x padding-t bg-black pb-8">Footer</footer>
       </main>
     </>
   );
